81623d4d3f088540c6be6428a58db5ffabcfbd3f
EliHeredia <elih0993@gmail.com>
2023-08-31 04:04:37 -0300

    Otro Hello World

--- src/main/java/com/mycompany/tp2/TP2.java
+++ src/main/java/com/mycompany/tp2/TP2.java
@@ -12,7 +12,7 @@ public class TP2 {
 
     public static void main(String[] args) {
         System.out.println("Hello World! Im Uriel Heredia!");
-        System.out.println("Hello World! Im Elizabeth Heredia!");
+        System.out.println("Hello World! Im Elizabeth Heredia!!");
         
     }
 }
